c2eb410b7ae2aeb717c50ad6bf2fd78aff4039bd
Youngseo Yoo <7dudtj@naver.com>
2021-09-23 18:01:30 +0900

    가독성 향상

--- Week1/stack/boj_17608.java
+++ Week1/stack/boj_17608.java
@@ -1,10 +1,12 @@
+// skeleton code of boj_17608
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
 // boj_17608 >> Main
 public class boj_17608 {
-	public static void main(String args[]) throws IOException	{
+    public static void main(String args[]) throws IOException {
     // set needed variables
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
