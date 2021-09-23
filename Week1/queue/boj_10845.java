972b0378940b526cb01a1163691727cd38e89629
Youngseo Yoo <7dudtj@naver.com>
2021-09-23 17:58:09 +0900

    가독성 향상

--- Week1/queue/boj_10845.java
+++ Week1/queue/boj_10845.java
@@ -1,3 +1,5 @@
+// skeleton code of boj_10845
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -5,74 +7,74 @@ import java.io.IOException;
 // boj_10845 >> Main
 public class boj_10845 {
 	public static void main(String args[]) throws IOException {
-		// set needed components
-		// myQueue myQ = new myQueue();
+	// set needed components
+	// myQueue myQ = new myQueue();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		String pro;
+	int N = Integer.parseInt(br.readLine());
+	String pro;
 		
-		// do process for N times
-		for (int i = 0; i < N; i++) {
-			// get command
-			pro = br.readLine();
+	// do process for N times
+	for (int i = 0; i < N; i++) {
+		// get command
+		pro = br.readLine();
 			
-			// process by command
-			// if command is 'PUSH'
-			if (pro.substring(0, 2).equals("pu")) {
-                int answer = 0;
-				int pushNum = Integer.parseInt(pro.substring(5));
-				/*
-                직접 해결해보세요!
-                */
-                sb.append(answer).append('\n');
-			}
-			// if command is 'POP'
-			else if (pro.equals("pop")) {
-                int answer = 0;
-				/*
-                직접 해결해보세요!
-                */
-                sb.append(answer).append('\n');
-			}
-			// if command is 'SIZE'
-			else if (pro.equals("size")){
-                int answer = 0;
-				/*
-                직접 해결해보세요!
-                */
-                sb.append(answer).append('\n');
-			}
-			// if command is 'EMPTY'
-			else if (pro.equals("empty")) {
-                int answer = 0;
-				/*
-                직접 해결해보세요!
-                */
-                sb.append(answer).append('\n');
-			}
-			// if command is 'FRONT'
-			else if (pro.equals("front")) {
-                int answer = 0;
-				/*
-                직접 해결해보세요!
-                */
-                sb.append(answer).append('\n');
-			}
-			// if command is 'BACK'
-			else if (pro.equals("back")) {
-                int answer = 0;
-				/*
-                직접 해결해보세요!
-                */
-                sb.append(answer).append('\n');
-			}
+		// process by command
+		// if command is 'PUSH'
+		if (pro.substring(0, 2).equals("pu")) {
+                	int answer = 0;
+			int pushNum = Integer.parseInt(pro.substring(5));
+			/*
+                	직접 해결해보세요!
+                	*/
+                	sb.append(answer).append('\n');
 		}
+		// if command is 'POP'
+		else if (pro.equals("pop")) {
+                	int answer = 0;
+			/*
+                	직접 해결해보세요!
+                	*/
+                	sb.append(answer).append('\n');
+		}
+		// if command is 'SIZE'
+		else if (pro.equals("size")){
+                	int answer = 0;
+			/*
+                	직접 해결해보세요!
+                	*/
+                	sb.append(answer).append('\n');
+		}
+		// if command is 'EMPTY'
+		else if (pro.equals("empty")) {
+                	int answer = 0;
+			/*
+                	직접 해결해보세요!
+                	*/
+                	sb.append(answer).append('\n');
+		}
+		// if command is 'FRONT'
+		else if (pro.equals("front")) {
+                	int answer = 0;
+			/*
+                	직접 해결해보세요!
+                	*/
+                	sb.append(answer).append('\n');
+		}
+		// if command is 'BACK'
+		else if (pro.equals("back")) {
+                	int answer = 0;
+			/*
+                	직접 해결해보세요!
+                	*/
+                	sb.append(answer).append('\n');
+		}
+	}
 		
-		// end program
+	// end program
         System.out.print(sb);
-		br.close();
-		return;
+	br.close();
+	return;
 	}
 }
 
